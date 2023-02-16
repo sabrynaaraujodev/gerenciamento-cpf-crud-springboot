82b41a9c8bd2d2529126f5ba99ab78245df1617b
Sabryna Araujo <sabrynaaraujodev@gmail.com>
2023-02-16 12:52:20 -0300

    novo metodo get

--- src/main/java/br/com/pamcary/controller/PessoaController.java
+++ src/main/java/br/com/pamcary/controller/PessoaController.java
@@ -1,8 +1,25 @@
 package br.com.pamcary.controller;
 
+import br.com.pamcary.entity.Pessoa;
+import br.com.pamcary.service.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RestController
 public class PessoaController {
+    @Autowired
+    PessoaService pessoaService;
+
+    @GetMapping("/")
+    public ModelAndView getAllPerson() {
+        ModelAndView mav = new ModelAndView("index");
+        List<Pessoa> peopleList = pessoaService.findAll();
+        mav.addObject("people", peopleList);
+        return mav;
+    }
 
 }
